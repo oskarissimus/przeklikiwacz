7e4e777bd61aa61e77a2ecdac69c5eb1bca2786a
oskarissimus <oskar.korczak@student.uwm.edu.pl>
2019-10-18 17:37:02 +0200

    added placeholders and controller structure for UI

--- przeklikiwacz2.0.py
+++ przeklikiwacz2.0.py
@@ -34,13 +34,18 @@ class Observable:
 
 class Model:
     def __init__(self):
-        self.currentAction = Observable("inicjacja currentAction")
+        self.currentAction    = Observable("inicjacja currentAction")
+        self.actionsEnabled   = Observable(["inicjacja ActionsEnabled"])
+        self.actionsAvailable = ['przeklikiwanie','pauza','edit','reload','quit','ready']
+
 
     def currentAction_set(self, action):
         self.currentAction.set(action)
 
 
-
+    def actionsEnabled_set(self, actions):
+        self.actionsEnabled.set(actions)
+        
 
 
 
@@ -60,6 +65,7 @@ class View(tk.Toplevel):
                                        font='Helvetica 18 bold',
                                        text='PRZEKLIKUJ')
         self.clickerButton.pack(side="top")
+
         
         
 
@@ -75,17 +81,33 @@ class View(tk.Toplevel):
 
         
 
-#        self.edit = tk.Button(self, text="edytuj plik konfiguracyjny",width=25,font='Helvetica 11', command=self.open_config)
-#        self.edit.pack()
-#
-#        self.reload = tk.Button(self, text="przeładuj konfigurację",width=25,font='Helvetica 11', command=self.reload_config)
-#        self.reload.pack()
-#
-#        self.quit = tk.Button(self, text="QUIT", font='Helvetica 11', command=self.master.destroy)
-#        self.quit.pack(side="bottom")
+        self.editButton = tk.Button(self,
+                                    text="edytuj plik konfiguracyjny",
+                                    width=25,
+                                    font='Helvetica 11')
+        self.editButton.pack(side="top")
+
+
+
+
+
+        self.reloadButton = tk.Button(self,
+                                      text="przeładuj konfigurację",
+                                      width=25,
+                                      font='Helvetica 11')
+        self.reloadButton.pack(side="top")
+
+
+
+
+
+        self.quitButton = tk.Button(self,
+                                    text="QUIT",
+                                    font='Helvetica 11')
+        self.quitButton.pack(side="top")
         
         self.infoBox = tk.Label(self,text="elo")
-        self.infoBox.pack(side="top")
+        self.infoBox.pack(side="bottom")
 
 
         
@@ -114,10 +136,15 @@ class Controller:
         ##przypinanie funkcji kontrolera do widoku
         self.view.clickerButton.config (command = lambda: self.currentAction_set('przeklikiwanie'))
         self.view.pauseButton.config   (command = lambda: self.currentAction_set('pauza'))
+        self.view.editButton.config    (command = lambda: self.currentAction_set('edit'))
+        self.view.reloadButton.config  (command = lambda: self.currentAction_set('reload'))
+        self.view.quitButton.config    (command = lambda: self.currentAction_set('quit'))
 
         ##przypinanie callback do observable        
-        self.model.currentAction.addCallback (self.currentAction_changed)
-        self.currentAction_changed('nic')
+        self.model.currentAction.addCallback  (self.currentAction_changed)
+
+        self.model.actionsEnabled.addCallback (self.actionsEnabled_changed)
+
         
     def browser_click_targets_in_all_urls(self):
         logging.debug ('klikam niby')
@@ -127,9 +154,78 @@ class Controller:
 
     def currentAction_changed(self, action):
         self.view.currentAction_display(action)
-        self.browser_click_targets_in_all_urls()
+        if action == 'przeklikiwanie' :
+            self.model.actionsEnabled_set(['pauza'])
+            self.browser_click_targets_in_all_urls()
+            
+            
+        if action == 'pauza' :
+            self.model.actionsEnabled_set(['przeklikiwanie','edit','reload','quit'])
+            self.browser_pause()
+            
+            
+        if action == 'edit' :
+            self.model.actionsEnabled_set([])
+            self.config_edit()
+            self.currentAction_set('ready')
+            
+            
+        if action == 'reload' :
+            self.model.actionsEnabled_set([])
+            self.config_reload()
+            self.currentAction_set('ready')
+            
+            
+        if action == 'ready' :
+            self.model.actionsEnabled_set(['przeklikiwanie','edit','reload','quit'])
+            #ready state to set after some actions
 
 
+        if action == 'quit' :
+            self.model.actionsEnabled_set([])
+            #quit program
+
+
+
+    def actionsEnabled_changed(self, actions):
+        if 'przeklikiwanie' in actions:
+            self.view.clickerButton.config(state="normal")
+        else:
+            self.view.clickerButton.config(state="disabled")
+
+
+        if 'pauza' in actions:
+            self.view.pauseButton.config(state="normal")
+        else:
+            self.view.pauseButton.config(state="disabled")
+
+
+        if 'edit' in actions:
+            self.view.editButton.config(state="normal")
+        else:
+            self.view.editButton.config(state="disabled")
+
+
+        if 'reload' in actions:
+            self.view.reloadButton.config(state="normal")
+        else:
+            self.view.reloadButton.config(state="disabled")
+
+
+        if 'quit' in actions:
+            self.view.quitButton.config(state="normal")
+        else:
+            self.view.quitButton.config(state="disabled")
+
+
+
+    def config_edit(self):
+        #open config file in notepad
+        return
+    
+    def config_reload(self):
+        #load configfile to program state
+        return
 
 
 
